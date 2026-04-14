ae45b41a5b72d4f386230c89366886ed5c948337
antoine <guingand.antoine@gmail.com>
2026-04-14 20:37:49 +0200

    cleanup

--- js/component.js
+++ js/component.js
@@ -154,7 +154,6 @@ export class Component {
      * Here we'll take the new state and side effects from the
      * server and use them to update the existing data that
      * users interact with, triggering reactive effects.
-     * @internal
      */
     processEffects(effects, request) {
         // This is for BC.
